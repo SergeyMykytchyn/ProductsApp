2833df3d3dcedb1b78ea55af540700112621d131
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2020-09-21 23:28:52 +0300

    feat: greetings from the test controller

---
+++ routes/product.route.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// Reqire the controllers WHICH ARE DID NOT CREATE YET!!
+const product_controller = require("../controllers/product.controller");
+
+// a simple test url to check that all of our files are commuicating correctly
+router.get("./test", product_controller.test);
+
+module.exports = router;
