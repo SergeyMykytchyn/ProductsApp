58b0dc5e02f6fd1bfa8fc22ae906dfbe8b79469f
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2020-09-21 23:57:00 +0300

    feat: route use

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const port = 1234;
+const product = require("./routes/product.route");
+
+app.use("./products", product);
 
 app.listen(port, () => {
   console.log("Server is running on port ", port);
