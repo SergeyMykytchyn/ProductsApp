5df38f8cc09bbccf9ae9d0933797e3ffd2c62f6f
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2020-09-22 00:52:35 +0300

    feat: mongoose connect

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const product = require("./routes/product.route");
 
 const app = express();
@@ -11,3 +12,11 @@ let port = 1234;
 app.listen(port, () => {
   console.log("Server is running on port", port);
 });
+
+let dev_db_url =
+  "mongodb+srv://huntermsd:lavachkin140@clusterme.gu4lt.gcp.mongodb.net/ClusterMe?retryWrites=true&w=majority";
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connecction;
+db.on("error", console.log.error.bind(console, "MongoDB connection error:"));
