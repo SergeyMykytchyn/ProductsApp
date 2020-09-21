2384b8a921cd95f5b03c5164a00d78e838ac1d9e
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2020-09-22 00:56:31 +0300

    feat: app use body-parser

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const product = require("./routes/product.route");
 
+const product = require("./routes/product.route");
 const app = express();
 
-app.use("./products", product);
-
-let port = 1234;
-
-app.listen(port, () => {
-  console.log("Server is running on port", port);
-});
-
+// Set up mongoose connection
+const mongoose = require("mongoose");
 let dev_db_url =
   "mongodb+srv://huntermsd:lavachkin140@clusterme.gu4lt.gcp.mongodb.net/ClusterMe?retryWrites=true&w=majority";
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
@@ -20,3 +13,13 @@ mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 let db = mongoose.connecction;
 db.on("error", console.log.error.bind(console, "MongoDB connection error:"));
+
+app.use("./products", product);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let port = 1234;
+
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
