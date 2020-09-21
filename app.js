3bb4ce72650dd9f9025b48544e8b9e4dd462509a
Sergey Mykytchyn <sergeymykytchyn@gmail.com>
2020-09-22 00:25:34 +0300

    feat: preview

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-const app = express();
-const port = 1234;
 const product = require("./routes/product.route");
 
+const app = express();
+
 app.use("./products", product);
+
+let port = 1234;
 
 app.listen(port, () => {
   console.log("Server is running on port", port);
--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,5 +2,5 @@ const Product = require("../models/product.model");
 
 // Simple version without validation or sanitation
 exports.test = function (req, res) {
-  res.send("Greeting from the test controller!");
+  res.send("Greetings from the Test controller!");
 };
